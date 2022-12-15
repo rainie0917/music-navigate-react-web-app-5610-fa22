10b2df12e1651ca8f767dcf8d87cf3143b5f07ab
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-12-14 21:15:41 -0800

    before meeting with Wenbo

--- src/services/songs-thunks.js
+++ src/services/songs-thunks.js
@@ -2,10 +2,10 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import * as service
   from "./songs-service"
 
-export const findSongsThunk = createAsyncThunk(
-    'songs/findSongs', async () =>
-        await service.findSongs()
-)
+// export const findSongsThunk = createAsyncThunk(
+//     'songs/findSongs', async () =>
+//         await service.findSongs()
+// )
 
 export const updateSongThunk =
     createAsyncThunk(
@@ -13,3 +13,18 @@ export const updateSongThunk =
         async (song) =>
          await service.updateSong(song)
     )
+
+export const findSongsThunk = createAsyncThunk(
+    'songs/findSongs', async (title,{getState}) =>{
+      const data = await service.findSongs()
+      const state = getState();
+      const currentUser = state.users.currentUser
+      for(let song of data){
+        song.likedByCurrentUser =  false
+        if(currentUser != null && song.whoLiked.indexOf(currentUser.username) > -1){
+          song.likedByCurrentUser = true
+        }
+      }
+      return data
+    }
+)
--- src/songs/songStats.js
+++ src/songs/songStats.js
@@ -1,31 +1,164 @@
 import {useDispatch, useSelector} from 'react-redux';
 import React from 'react';
 // import {updateSongThunk} from "../services/songs-thunks";
-import {updateSongThunk} from "../services/search-thunks"
+import {createSongThunk, updateSongThunk} from "../services/search-thunks"
 import {updateUserThunk} from "../services/users-thunks";
+import * as service from "../services/search-service";
 
 const SongStats = ({ song }) => {
-  console.log(song)
+
+  // console.log(song)
   const dispatch = useDispatch();
   const {currentUser} = useSelector((state) => state.users)
-  const unLikeASong = async (track) =>{
-    let whoLikedArr = track.whoLiked.filter(i => i !== currentUser.username)
-    console.log(whoLikedArr)
-    dispatch(updateSongThunk([{
-      artist: track.artist,
-      realImg: track.realImg,
-      mbid: track.mbid,
-      name: track.name,
-      whoLiked: whoLikedArr,
-      likes: track.likes - 1,
-    }, false]))
-    let likedSongsArr = currentUser.likedSongs.filter(i => i !== track.mbid)
-    const updatedUser = {
-      ...currentUser,
-      likedSongs: likedSongsArr
+
+  // const unLikeASong = async (track) =>{
+  //   let whoLikedArr = track.whoLiked.filter(i => i !== currentUser.username)
+  //   console.log(whoLikedArr)
+  //   dispatch(updateSongThunk([{
+  //     artist: track.artist,
+  //     realImg: track.realImg,
+  //     mbid: track.mbid,
+  //     name: track.name,
+  //     whoLiked: whoLikedArr,
+  //     likes: track.likes - 1,
+  //   }, false]))
+  //   let likedSongsArr = currentUser.likedSongs.filter(i => i !== track.mbid)
+  //   const updatedUser = {
+  //     ...currentUser,
+  //     likedSongs: likedSongsArr
+  //   }
+  //   dispatch(updateUserThunk(updatedUser))
+  // }
+
+  const likeASong = async (track) => {
+    // if user logged in
+    if(currentUser != null){
+      let songInfo = await service.searchSongInDB(track.mbid)
+      // if song in DB, like += 1
+      if(songInfo != null){
+        let whoLikedArr = JSON.parse(JSON.stringify(track.whoLiked))
+        whoLikedArr.push(currentUser.username)
+        dispatch(updateSongThunk([{
+          // ...track,
+          artist: track.artist,
+          realImg: track.realImg,
+          mbid: track.mbid,
+          name: track.name,
+          whoLiked: whoLikedArr,
+          likes: track.likes + 1,
+          // liked: true,
+        }, true]))
+        let likedSongsArr = JSON.parse(JSON.stringify(currentUser.likedSongs))
+        likedSongsArr.push(track.mbid)
+        const updatedUser = {
+          ...currentUser,
+          likedSongs: likedSongsArr
+        }
+        dispatch(updateUserThunk(updatedUser))
+      }
+      // if song not in DB, create song in DB
+      else{
+        dispatch(createSongThunk([{
+          artist: track.artist,
+          realImg: track.realImg,
+          mbid: track.mbid,
+          name: track.name,
+          whoLiked: [currentUser.username],
+          likes: 1,
+        }, true]))
+        let likedSongsArr = JSON.parse(JSON.stringify(currentUser.likedSongs))
+        likedSongsArr.push(track.mbid)
+        const updatedUser = {
+          ...currentUser,
+          likedSongs: likedSongsArr
+        }
+        dispatch(updateUserThunk(updatedUser))
+      }
     }
-    dispatch(updateUserThunk(updatedUser))
+    else{
+      alert("Please log in to like/unlike a song!")
+    }
   }
+
+  const unLikeASong = async (track) =>{
+    if(currentUser != null) {
+      let whoLikedArr = track.whoLiked.filter(i => i !== currentUser.username)
+      dispatch(updateSongThunk([{
+        artist: track.artist,
+        realImg: track.realImg,
+        mbid: track.mbid,
+        name: track.name,
+        whoLiked: whoLikedArr,
+        likes: track.likes - 1,
+      }, false]))
+      let likedSongsArr = currentUser.likedSongs.filter(i => i !== track.mbid)
+      const updatedUser = {
+        ...currentUser,
+        likedSongs: likedSongsArr
+      }
+      dispatch(updateUserThunk(updatedUser))
+    }
+    else{
+      alert("Please log in to like/unlike a song!")
+    }
+  }
+
+
+  // return (
+  //     <div className='col-12 d-flex pt-2 pe-5 justify-content-between'>
+  //       <div>
+  //         {
+  //             song.liked && currentUser &&
+  //             <button
+  //                 onClick={() => unLikeASong(song)}
+  //                 // onClick={() => dispatch(updateSongThunk([{
+  //                 //   ...song,
+  //                 //   likes: song.likes - 1,
+  //                 // }, false]))}
+  //                 className='btn btn-sm'>
+  //               {/*<i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.whoLiked.length}*/}
+  //               <i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.likes}
+  //             </button>
+  //         }
+  //         {/*console.log(song.whoLiked.length)*/}
+  //         {
+  //             song.liked && !currentUser &&
+  //             <button
+  //                 // onClick={alert("Please Login")}
+  //                 className='btn btn-sm'>
+  //               {/*<i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.whoLiked.length}*/}
+  //               <i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.likes}
+  //             </button>
+  //         }
+  //
+  //         {
+  //             !song.liked && currentUser &&
+  //             <button
+  //                 onClick={() => dispatch(updateSongThunk({
+  //                   ...song,
+  //                   likes: typeof song.likes === 'undefined' ? 1: song.likes + 1,
+  //                   liked: true
+  //                 }))}
+  //                 className='btn btn-sm'>
+  //               {/*<i className='fa-regular fa-heart'/> {song.whoLiked.length}*/}
+  //               <i className='fa-regular fa-heart'/> {song.likes}
+  //             </button>
+  //         }
+  //
+  //         {
+  //             !song.liked && !currentUser &&
+  //             <button
+  //                 // onClick={alert("Please Login")}
+  //                 className='btn btn-sm'>
+  //               {/*<i className='fa-regular fa-heart'/> {song.whoLiked.length}*/}
+  //               <i className='fa-regular fa-heart'/> {song.likes}
+  //             </button>
+  //         }
+  //
+  //
+  //       </div>
+  //     </div>
+  // )
 
   return (
       <div className='col-12 d-flex pt-2 pe-5 justify-content-between'>
@@ -34,16 +167,11 @@ const SongStats = ({ song }) => {
               song.liked && currentUser &&
               <button
                   onClick={() => unLikeASong(song)}
-                  // onClick={() => dispatch(updateSongThunk([{
-                  //   ...song,
-                  //   likes: song.likes - 1,
-                  // }, false]))}
                   className='btn btn-sm'>
-                {/*<i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.whoLiked.length}*/}
                 <i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.likes}
               </button>
           }
-          {/*console.log(song.whoLiked.length)*/}
+
           {
               song.liked && !currentUser &&
               <button
@@ -78,56 +206,16 @@ const SongStats = ({ song }) => {
               </button>
           }
 
-          {/*{*/}
-          {/*    song.liked &&*/}
-          {/*    <button*/}
-          {/*        onClick={() => dispatch(updateSongThunk({*/}
-          {/*          ...song,*/}
-          {/*          likes: song.likes - 1,*/}
-          {/*          liked: false*/}
-          {/*        }))}*/}
-          {/*        className='btn btn-sm'>*/}
-          {/*      <i className='fa-solid fa-heart  fs-6 text-danger' style={{color: 'red'}}/> {song.likes}*/}
-          {/*    </button>*/}
-          {/*}*/}
-
-          {/*{*/}
-          {/*    !song.liked &&*/}
-          {/*    <button*/}
-          {/*        onClick={() => dispatch(updateSongThunk({*/}
-          {/*          ...song,*/}
-          {/*          likes: typeof song.likes === 'undefined' ? 1: song.likes + 1,*/}
-          {/*          liked: true*/}
-          {/*        }))}*/}
-          {/*        className='btn btn-sm'>*/}
-          {/*      <i className='fa-regular fa-heart'/> {song.likes}*/}
-          {/*    </button>*/}
-          {/*}*/}
-
-          {/*cancel like*/}
-          {/*{song.liked && <i onClick={() => dispatch(updateSongThunk({*/}
-          {/*  ...song,*/}
-          {/*  likes: song.likes - 1,*/}
-          {/*  liked: false,*/}
-          {/*}))}*/}
-          {/*                  className="fa-solid fa-heart  fs-6 text-danger"/>}*/}
-          {/*/!*likes a tuit*!/*/}
-          {/*{!song.liked && <i onClick={() => dispatch(updateSongThunk({*/}
-          {/*  ...song,*/}
-          {/*  likes: song.likes + 1,*/}
-          {/*  liked: true,*/}
-          {/*}))}*/}
-          {/*                   className="fa-regular fa-heart  fs-6 fw-light"/>}*/}
-          {/*<span className="ps-2 "*/}
-          {/*      style={{"verticalAlign": "top"}}> {song.likes} </span>*/}
 
         </div>
       </div>
+  )
 
 
 
 
-  );
+
+
 };
 
 export default SongStats;
