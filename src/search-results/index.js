9ebd89a2a74a8ed2d7e041ccf4774da1a49a9e23
Wenbo Pan <wenbopan313@gmail.com>
2022-12-14 16:24:58 -0800

    add loading

--- src/search-results/index.js
+++ src/search-results/index.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '@fortawesome/fontawesome-free/css/all.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import TrackItem from "./track-item";
+import {searchSongThunk} from "../services/search-thunks";
 
 
 const ResultComponent = () => {
 	const search = useSelector(state => state.search)
-	
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(searchSongThunk())
+	}, [])
+	const loading = useSelector(state => state.search.loading)
 	return(
 		<>
 				<h2>Search result for "{search.title}"</h2>
 				<ul className="list-group">
-				{
+					{
+						loading &&
+						<li className="list-group-item">
+							Loading...
+						</li>
+					}
+					
+				{ !loading &&
 					search.tracks.map(track =>
 					<TrackItem key={track.url} track={track} >
 					</TrackItem>)
